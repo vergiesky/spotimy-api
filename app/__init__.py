0939ff88bf6904dd706de068e04629092a164377
vergiesky <vergiethie65@gmail.com>
2026-07-18 19:19:17 +0700

    set up flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from app.extensions import bcrypt, db, migrate
+from config import Config
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(config_class)
 
-    @app.get("/")
-    def index ():
-        return {
-            "message": "running"
-        }
+    # Initialize Flask extension
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    
+    @app.get("/health")
+    def health():
+        return "OK", 200
 
     return app
