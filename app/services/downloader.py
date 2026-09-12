9b4dbd79521bb83b03e3229bdd472356dd1af653
vergiesky <vergiethie65@gmail.com>
2026-09-12 23:55:03 +0700

    fix(downloader): use web and android youtube clients

--- app/services/downloader.py
+++ app/services/downloader.py
@@ -123,6 +123,11 @@ def download_audio(url):
         "noplaylist": True,
         "quiet": True,
         "remote_components": ["ejs:github"],
+        "extractor_args": {
+        "youtube": {
+            "player_client": ["web", "android"],
+        },
+    },
     }
 
     if cookie_path:
